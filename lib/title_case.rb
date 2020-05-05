69af2cebed346d70034775ac6245371f65583337
Ryan Duff <ryanduff613@gmail.com>
2020-05-04 21:11:57 -0700

    add require pry to title_case.rb

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,3 +1,11 @@
-def title_case(title)
-  title.capitalize!
+require('pry')
+
+class String
+  def title_case
+    split_sentence = self.split()
+    split_sentence.each() do |word|
+      word.capitalize!()
+    end
+    split_sentence.join(" ")
+  end
 end
